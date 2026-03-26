b2a472ee5759e4b0a8c04af3f03a493d472adace
Ansh <ansh@portfolio.dev>
2026-03-26 23:35:10 +0000

    Fix: scroll to top on load, disable browser scroll restoration

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import Footer from "@/components/Footer";
 import NoiseBgLoader from "@/components/NoiseBgLoader";
 import CursorBlobLoader from "@/components/CursorBlobLoader";
 import ScrollProgress from "@/components/ScrollProgress";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export default function Home() {
   return (
     <>
+      <ScrollToTop />
       <ScrollProgress />
       <NoiseBgLoader />
       <CursorBlobLoader />
